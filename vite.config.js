5994ded82ff99a3f209f36569ddc8649dded0d92
Paulo Pedroso <pclpedroso@gmail.com>
2026-08-21 00:16:39 -0300

    fix: set relative base path for perfect styling in desktop Electron app

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', // Permite que os assets CSS/JS carreguem perfeitamente tanto no Electron (file://) quanto na web
   server: {
     host: '127.0.0.1',
     port: 5173,
